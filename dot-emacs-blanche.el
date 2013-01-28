1ee0a73b790556354b9f9c1fef78258014cb4992
Karl Voit <git@Karl-Voit.at>
2013-01-28 11:54:49 +0100

    accept flyspell as save

--- dot-emacs-blanche.el
+++ dot-emacs-blanche.el
@@ -18,9 +18,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(initial-buffer-choice t)
- '(org-contacts-files "~/share/all/org-mode/contacts.org")
- '(safe-local-variable-values (quote ((eval ispell-change-dictionary "en_US") (TeX-master . "../main")))))
+ '(safe-local-variable-values (quote ((eval ispell-change-dictionary "en_US") (eval flyspell-mode 1)))))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
