dfee5fa1f2e9ea6704e4fee681e329b4b875e83f
Karl Voit <git@Karl-Voit.at>
2014-01-24 16:45:16 +0100

    minor Windows fixes

--- init-atgrz4043268b.el
+++ init-atgrz4043268b.el
@@ -12,12 +12,12 @@
 ;; ######################################################
 ;; http://www.tbray.org/ongoing/When/201x/2012/09/24/Typographic-notes
 ;; setting font size
-(custom-set-faces
+;(custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+; )
 
 
 ;; END OF FILE ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
