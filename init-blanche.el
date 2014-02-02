c9cfdbb60635c84f96368087465e846750a6aabe
Karl Voit <git@Karl-Voit.at>
2014-02-02 11:12:14 +0100

    added custom variables

--- init-blanche.el
+++ init-blanche.el
@@ -14,14 +14,27 @@
 ;; ######################################################
 ;; END OF FILE ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(safe-local-variable-values (quote ((eval ispell-change-dictionary "american") (eval ispell-change-dictionary "german8") (flyspell-default-dictionary . "german8") (eval ispell-change-dictionary "de_AT") (eval ispell-change-dictionary "en_US") (eval flyspell-mode 1)))))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t)
+ '(org-contacts-address-property "CITY")
+ '(org-contacts-birthday-property "BORN")
+ '(org-contacts-files "~/share/all/org-mode/contacts.org")
+ '(org-contacts-icon-property "PHOTOGRAPH")
+ '(safe-local-variable-values
+   (quote
+    ((flyspell-default-dictionary . "en_US")
+     (eval ispell-change-dictionary "american")
+     (eval ispell-change-dictionary "german8")
+     (flyspell-default-dictionary . "german8")
+     (eval ispell-change-dictionary "de_AT")
+     (eval ispell-change-dictionary "en_US")
+     (eval flyspell-mode 1)))))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  )
