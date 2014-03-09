a76b2cc81282982dd11f54235db1b047d360d4f3
Karl Voit <git@Karl-Voit.at>
2014-03-09 12:39:53 +0100

    cleaned up mixup of two host-specific init files

--- init-blanche.el
+++ init-blanche.el
@@ -18,6 +18,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(fci-rule-color "#383838")
  '(inhibit-startup-screen t)
  '(org-contacts-address-property "CITY")
  '(org-contacts-birthday-property "BORN")
@@ -31,7 +32,29 @@
      (flyspell-default-dictionary . "german8")
      (eval ispell-change-dictionary "de_AT")
      (eval ispell-change-dictionary "en_US")
-     (eval flyspell-mode 1)))))
+     (eval flyspell-mode 1))))
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#BC8383")
+     (40 . "#CC9393")
+     (60 . "#DFAF8F")
+     (80 . "#D0BF8F")
+     (100 . "#E0CF9F")
+     (120 . "#F0DFAF")
+     (140 . "#5F7F5F")
+     (160 . "#7F9F7F")
+     (180 . "#8FB28F")
+     (200 . "#9FC59F")
+     (220 . "#AFD8AF")
+     (240 . "#BFEBBF")
+     (260 . "#93E0E3")
+     (280 . "#6CA0A3")
+     (300 . "#7CB8BB")
+     (320 . "#8CD0D3")
+     (340 . "#94BFF3")
+     (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init-gary.el
+++ init-gary.el
@@ -26,9 +26,20 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   (quote
+    ((flyspell-default-dictionary . "en_US")
+     (eval ispell-change-dictionary "american")
+     (eval ispell-change-dictionary "german8")
+     (flyspell-default-dictionary . "german8")
+     (eval ispell-change-dictionary "de_AT")
+     (eval ispell-change-dictionary "en_US")
+     (eval flyspell-mode 1))))
+ '(ansi-color-names-vector
+   ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
+ '(custom-safe-themes
+   (quote
+    ("d070fa185078bf753dcfd873ec63be19fa36a55a0c97dc66848a6d20c5fffdad" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" default)))
  '(inhibit-startup-screen t)
- '(safe-local-variable-values (quote ((OLDeval ispell-change-dictionary "german8") (flyspell-default-dictionary . "german8") (eval ispell-change-dictionary "german8") (eval ispell-change-dictionary "american") (emerge-set-combine-versions-template . "%a
-%b
-") (eval hide-body) (eval end-of-buffer) (eval flyspell-mode -1) (eval auto-fill-mode -1) (eval ispell-change-dictionary "de_AT") (eval flyspell-mode 1) (eval ispell-change-dictionary "en_US")))))
-
+)
 
