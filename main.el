f4b33ea4bd9f2a23b3765e52b5436b6d876f6a05
Karl Voit <git@Karl-Voit.at>
2014-08-13 21:34:26 +0200

    F12 = naked emacs (fullscreen and so on)

--- main.el
+++ main.el
@@ -3001,6 +3001,165 @@ by using nxml's indentation rules."
 (global-set-key (kbd "<C-return>") 'yafolding-toggle-element)
 
 
+;; ######################################################
+;; full-screen, naked Emacs without distractions
+;; thanks to Bastien; adopted to my requirements
+;; https://gist.github.com/bzg/8578998
+
+(defvar my-toggle-naked-emacs-status nil
+  "state of fullscreen/naked Emacs mode. t means fullscreen, nil means normal")
+(make-variable-buffer-local 'my-toggle-naked-emacs-status)
+
+;; See http://bzg.fr/emacs-hide-mode-line.html
+(defvar-local hidden-mode-line-mode nil)
+(defvar-local hide-mode-line nil)
+
+(define-minor-mode hidden-mode-line-mode
+  "Minor mode to hide the mode-line in the current buffer."
+  :init-value nil
+  :global nil
+  :variable hidden-mode-line-mode
+  :group 'editing-basics
+  (if hidden-mode-line-mode
+      (setq hide-mode-line mode-line-format
+            mode-line-format nil)
+    (setq mode-line-format hide-mode-line
+          hide-mode-line nil))
+  (force-mode-line-update)
+  ;; Apparently force-mode-line-update is not always enough to
+  ;; redisplay the mode-line
+  (redraw-display)
+  (when (and (called-interactively-p 'interactive)
+             hidden-mode-line-mode)
+    (run-with-idle-timer
+     0 nil 'message
+     (concat "Hidden Mode Line Mode enabled.  "
+             "Use M-x hidden-mode-line-mode to make the mode-line appear."))))
+
+;; A small minor mode to use a big fringe
+(defvar bzg-big-fringe-mode nil)
+(define-minor-mode bzg-big-fringe-mode
+  "Minor mode to hide the mode-line in the current buffer."
+  :init-value nil
+  :global t
+  :variable bzg-big-fringe-mode
+  :group 'editing-basics
+  (if (not bzg-big-fringe-mode)
+      (set-fringe-style nil)
+    (set-fringe-mode
+     (/ (- (frame-pixel-width)
+           (* 100 (frame-char-width)))
+        3)
+     )
+    ))
+
+;; Command to toggle the display of the mode-line as a header
+(defvar-local header-line-format nil)
+(defun mode-line-in-header ()
+  (interactive)
+  (if (not header-line-format)
+      (setq header-line-format mode-line-format
+            mode-line-format nil)
+    (setq mode-line-format header-line-format
+          header-line-format nil))
+  (set-window-buffer nil (current-buffer)))
+
+(defun my-toggle-naked-emacs ()
+  "Toggle fullscreen/naked Emacs and normal Emacs"
+  (interactive)
+  (cond (my-toggle-naked-emacs-status
+	 ;; make it naked!
+	 
+	 (setq my-toggle-naked-emacs-status nil)
+	 ;; Prevent the cursor from blinking
+	 (blink-cursor-mode 0)
+	 ;; Don't let Emacs hurt your ears
+	 (setq visible-bell t)
+
+	 ;; This is bound to f11 in Emacs 24.4
+	 (toggle-frame-fullscreen) 
+	 ;; ;; Who use the bar to scroll?
+	 ;; (scroll-bar-mode 0)
+
+	 (menu-bar-mode 0)
+
+	 ;; You can also set the initial frame parameters
+	 ;; (setq initial-frame-alist
+	 ;;       '((menu-bar-lines . 0)
+	 ;;         (tool-bar-lines . 0)))
+
+	 ;; Activate hidden-mode-line-mode
+	 (hidden-mode-line-mode 1)
+
+	 ;; If you want to hide the mode-line in all new buffers
+	 ;; (add-hook 'after-change-major-mode-hook 'hidden-mode-line-mode)
+
+	 ;; Alternatively, you can paint your mode-line in White but then
+	 ;; you'll have to manually paint it in black again
+	 ;; (custom-set-faces
+	 ;;  '(mode-line-highlight ((t nil)))
+	 ;;  '(mode-line ((t (:foreground "white" :background "white"))))
+	 ;;  '(mode-line-inactive ((t (:background "white" :foreground "white")))))
+
+	 ;; reset fringe with:
+	 ;; (set-fringe-mode nil)
+
+	 ;; Now activate this global minor mode
+	 (bzg-big-fringe-mode 1)
+
+	 ;; To activate the fringe by default and deactivate it when windows
+	 ;; are split vertically, uncomment this:
+	 ;; (add-hook 'window-configuration-change-hook
+	 ;;           (lambda ()
+	 ;;             (if (delq nil
+	 ;;                       (let ((fw (frame-width)))
+	 ;;                         (mapcar (lambda(w) (< (window-width w) fw))
+	 ;;                                 (window-list))))
+	 ;;                 (bzg-big-fringe-mode 0)
+	 ;;               (bzg-big-fringe-mode 1))))
+
+	 ;; Use a minimal cursor
+	 ;; (setq cursor-type 'hbar)
+
+	 ;; ;; Get rid of the indicators in the fringe
+	 ;; (mapcar (lambda(fb) (set-fringe-bitmap-face fb 'org-hide))
+	 ;;         fringe-bitmaps)
+	 ;; 
+	 ;; ;; Set the color of the fringe
+	 ;; (custom-set-faces
+	 ;;  '(fringe ((t (:background "white")))))
+	 ;; 
+	 ;; (custom-set-faces
+	 ;;   '(default ((t (:background "black" :foreground "grey"))))
+	 ;;   '(fringe ((t (:background "black")))))
+
+	 ;;(global-set-key (kbd "C-s-SPC") 'mode-line-in-header)
+	 (define-key my-map "h" 'mode-line-in-header)
+
+	 (message "Enjoy your concentration!")
+	 
+	 )
+	(t
+	 ;; normal mode
+	 (setq my-toggle-naked-emacs-status t)
+
+	 (blink-cursor-mode t)
+	 (setq visible-bell nil)
+	 (toggle-frame-fullscreen) 
+	 (scroll-bar-mode 1)
+	 (menu-bar-mode 1)
+	 (hidden-mode-line-mode nil)
+
+	 ;; If you want to hide the mode-line in all new buffers
+	 ;; (add-hook 'after-change-major-mode-hook 'hidden-mode-line-mode)
+
+	 (bzg-big-fringe-mode nil)
+	 (set-fringe-mode nil)
+
+	 (message "See everything.")
+	 )
+	)
+  )
 
 
 
@@ -3023,6 +3182,8 @@ by using nxml's indentation rules."
 (global-set-key [end]  'end-of-buffer)
 
 (define-key my-map " " 'delete-trailing-whitespace)
+
+(global-set-key [f12] 'my-toggle-naked-emacs)
 
 ;; ######################################################
 ;; coping with ELISP code
