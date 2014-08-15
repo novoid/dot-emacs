14f243a3192e40176cb03ef6e1506f441ab83573
Karl Voit <git@Karl-Voit.at>
2014-08-15 12:39:51 +0200

    my-open-in-external-app from ergoemacs

--- main.el
+++ main.el
@@ -3164,6 +3164,43 @@ by using nxml's indentation rules."
     )
 )
 
+;; ######################################################
+;; http://ergoemacs.org/emacs/emacs_dired_open_file_in_ext_apps.html
+;; open dired file in external app (specified by the operating system)
+(defun my-open-in-external-app (&optional file)
+  "Open the current file or dired marked files in external app.
+
+The app is chosen from your OS's preference."
+  (interactive)
+  (let ( doIt
+         (myFileList
+          (cond
+           ((string-equal major-mode "dired-mode") (dired-get-marked-files))
+           ((not file) (list (buffer-file-name)))
+           (file (list file)))))
+    
+    (setq doIt (if (<= (length myFileList) 5)
+                   t
+                 (y-or-n-p "Open more than 5 files? ") ) )
+    
+    (when doIt
+      (cond
+       ((string-equal system-type "windows-nt")
+        (mapc (lambda (fPath) (w32-shell-execute "open" (replace-regexp-in-string "/" "\\" fPath t t)) ) myFileList))
+       ((string-equal system-type "darwin")
+        (mapc (lambda (fPath) (shell-command (format "open \"%s\"" fPath)) )  myFileList) )
+       ((string-equal system-type "gnu/linux")
+        (mapc (lambda (fPath) (let ((process-connection-type nil)) (start-process "" nil "xdg-open" fPath)) ) myFileList) ) ) ) ) )
+
+
+
+
+
+
+
+
+
+
 
 
 
