f083800984482e1562217f2986546c3547e0fb3b
Karl Voit <git@Karl-Voit.at>
2014-11-17 19:11:49 +0100

    new version of defadvice org-archive-subtree

--- main.el
+++ main.el
@@ -2336,29 +2336,30 @@ Null prefix argument turns off the mode."
   
   ;; ######################################################
 ;;** preserve top level node and tags when archiving
-  ;; http://orgmode.org/worg/org-hacks.html#sec-1_3
+  ;; http://orgmode.org/worg/org-hacks.html#sec-1-7-1
   (defun my-org-inherited-no-file-tags ()
     (let ((tags (org-entry-get nil "ALLTAGS" 'selective))
-	  (ltags (org-entry-get nil "TAGS")))
+          (ltags (org-entry-get nil "TAGS")))
       (mapc (lambda (tag)
-	      (setq tags
-		    (replace-regexp-in-string (concat tag ":") "" tags)))
-	    (append org-file-tags (when ltags (split-string ltags ":" t))))
+              (setq tags
+                    (replace-regexp-in-string (concat tag ":") "" tags)))
+            (append org-file-tags (when ltags (split-string ltags ":" t))))
       (if (string= ":" tags) nil tags)))
+  
   (defadvice org-archive-subtree (around my-org-archive-subtree-low-level activate)
     (let ((tags (my-org-inherited-no-file-tags))
-	  (org-archive-location
-	   (if (save-excursion (org-back-to-heading)
-			       (> (org-outline-level) 1))
-	       (concat (car (split-string org-archive-location "::"))
-		       "::* "
-		       (car (org-get-outline-path)))
-	     org-archive-location)))
+          (org-archive-location
+           (if (save-excursion (org-back-to-heading)
+                               (> (org-outline-level) 1))
+               (concat (car (split-string org-archive-location "::"))
+                       "::* "
+                       (car (org-get-outline-path)))
+             org-archive-location)))
       ad-do-it
       (with-current-buffer (find-file-noselect (org-extract-archive-file))
-	(save-excursion
-	  (while (org-up-heading-safe))
-	  (org-set-tags-to tags)))))
+        (save-excursion
+          (while (org-up-heading-safe))
+          (org-set-tags-to tags)))))
 
 
 ;;** org-link
