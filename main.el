2350459f015e42e72da45b398bae2b7258fde07f
Karl Voit <git@Karl-Voit.at>
2015-06-16 21:43:46 +0200

    updated title-capitalization()

--- main.el
+++ main.el
@@ -3771,15 +3771,12 @@ The app is chosen from your OS's preference."
 
 ;; #############################################################################
 ;;** Proper English Title Capitalization of a Marked Region
-(defvar my-do-not-capitalize-words '("a" "ago" "an" "and" "as" "at" "but" "by" "for"
-                         "from" "in" "into" "it" "next" "nor" "of" "off"
-                         "on" "onto" "or" "over" "past" "so" "the" "till"
-                         "to" "up" "yet"
-                         "n" "t" "es" "s"
-                         "lazyblorg" "mutt" "TextSecure")
-  "Words that are not capitalized in titles.")
+;; additionally to the list defined in title-capitalization:
+(defvar my-do-not-capitalize-words '("lazyblorg" "mutt")
+  "My personal list of words that doesn't get capitalized in titles.")
 
-(defun my-title-capitalization (beg end)
+
+(defun title-capitalization (beg end)
   "Proper English title capitalization of a marked region"
   ;; - before: the presentation of this heading of my own from my keyboard and yet
   ;; - after:  The Presentation of This Heading of My Own from My Keyboard and Yet
@@ -3787,33 +3784,50 @@ The app is chosen from your OS's preference."
   ;; - after:  A a a a a a a A
   (interactive "r")
   (save-excursion
-    ;; go to begin of first word:
-    (goto-char beg)
-    (capitalize-word 1)
-    (while (< (point) end)
-      (skip-syntax-forward "^w" end)
-      (let ((word (thing-at-point 'word t)))
-        (if (stringp word)
-            ;; capitalize each word in between except it is list member:
-            (if (member (downcase word) my-do-not-capitalize-words)
-                (downcase-word 1)
-              (capitalize-word 1)))))
-    ;; capitalize last word in any case:
-    (backward-word 1)
-    (if (and (>= (point) beg)
-             (not (member (or (thing-at-point 'word t) "s")
-                          '("n" "t" "es" "s"))))
-        (capitalize-word 1))))
+    (let* (
+	   ;; basic list of words which don't get capitalized according to simplified rules:
+	   ;; http://karl-voit.at/2015/05/25/elisp-title-capitalization/
+           (do-not-capitalize-basic-words '("a" "ago" "an" "and" "as" "at" "but" "by" "for"
+                                            "from" "in" "into" "it" "next" "nor" "of" "off"
+                                            "on" "onto" "or" "over" "past" "so" "the" "till"
+                                            "to" "up" "yet"
+                                            "n" "t" "es" "s"))
+	   ;; if user has defined 'my-do-not-capitalize-words, append to basic list:
+           (do-not-capitalize-words (if (boundp 'my-do-not-capitalize-words)
+                                        (append do-not-capitalize-basic-words my-do-not-capitalize-words )
+                                      do-not-capitalize-basic-words
+                                      )
+                                    )
+           )
+      ;; go to begin of first word:
+      (goto-char beg)
+      (capitalize-word 1)
+      ;; go through the region, word by word:
+      (while (< (point) end)
+        (skip-syntax-forward "^w" end)
+        (let ((word (thing-at-point 'word t)))
+          (if (stringp word)
+              ;; capitalize current word except it is list member:
+              (if (member (downcase word) do-not-capitalize-words)
+                  (downcase-word 1)
+                (capitalize-word 1)))))
+      ;; capitalize last word in any case:
+      (backward-word 1)
+      (if (and (>= (point) beg)
+               (not (member (or (thing-at-point 'word t) "s")
+                            '("n" "t" "es" "s"))))
+          (capitalize-word 1)))))
+
 
 (ert-deftest my-title-capitalization ()
-  "Tests proper English title capitalization; FIXXME: doesn't work yet"
+  "Tests proper English title capitalization"
   (should (string= (with-temp-buffer
 		     (insert "the presentation of this heading of my own from my keyboard and yet\n")
 		     (goto-char (point-min))
 		     (set-mark-command nil)
 		     (goto-char (point-max))
 		     ;(transient-mark-mode 1)
-		     (my-title-capitalization)
+		     (title-capitalization)
 		     (buffer-string))
 		   "The Presentation of This Heading of My Own from My Keyboard and Yet\n"
 		   )))
@@ -4163,7 +4177,7 @@ The app is chosen from your OS's preference."
 (define-key my-map "C" 'my-fix-drawer-order)
 
 ;;** my-title-capitalization (my-map C)
-(define-key my-map "c" 'my-title-capitalization)
+(define-key my-map "c" 'title-capitalization)
 
 ;;** hippie-expand (M-/)
 ;; http://emacswiki.org/emacs/HippieExpand
