3b81863caabb21a44baad00614ccae269e63a651
Karl Voit <git@Karl-Voit.at>
2016-01-08 14:36:24 +0100

    my-toggle-windows-split

--- main.el
+++ main.el
@@ -4174,6 +4174,39 @@ The app is chosen from your OS's preference."
 		   )))
 
 ;; #############################################################################
+;;** my-toggle-windows-split
+;; http://www.emacswiki.org/emacs/ToggleWindowSplit
+(defun my-toggle-windows-split ()
+  "Switch window split from horizontally to vertically, or vice versa.
+
+i.e. change right window to bottom, or change bottom window to right."
+  (interactive)
+  (require 'windmove)
+  (let ((done))
+    (dolist (dirs '((right . down) (down . right)))
+      (unless done
+        (let* ((win (selected-window))
+               (nextdir (car dirs))
+               (neighbour-dir (cdr dirs))
+               (next-win (windmove-find-other-window nextdir win))
+               (neighbour1 (windmove-find-other-window neighbour-dir win))
+               (neighbour2 (if next-win (with-selected-window next-win
+                                          (windmove-find-other-window neighbour-dir next-win)))))
+          ;;(message "win: %s\nnext-win: %s\nneighbour1: %s\nneighbour2:%s" win next-win neighbour1 neighbour2)
+          (setq done (and (eq neighbour1 neighbour2)
+                          (not (eq (minibuffer-window) next-win))))
+          (if done
+              (let* ((other-buf (window-buffer next-win)))
+                (delete-window next-win)
+                (if (eq nextdir 'right)
+                    (split-window-vertically)
+                  (split-window-horizontally))
+                (set-window-buffer (windmove-find-other-window neighbour-dir) other-buf))))))))
+
+(bind-key "|" #'my-toggle-windows-split my-map)
+
+
+;; #############################################################################
 ;;* Key bindings
 
 ;;** general navigation keys
