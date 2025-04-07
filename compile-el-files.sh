ca56d92a042ea22cb1eb5d32d38156ab906f5170
Karl Voit <git@Karl-Voit.at>
2025-04-07 18:22:32 +0200

    compile-el-files.sh: removed self-compiling org part

--- compile-el-files.sh
+++ compile-el-files.sh
@@ -11,9 +11,9 @@ for file in *.el;do echo "(byte-compile-file \"/home/vk/.emacs.d/$file\")" >> ${
 for file in contrib/*.el;do echo "(byte-compile-file \"/home/vk/.emacs.d/$file\")" >> ${FILE_WITH_EL_FILES}; done
 
 ## compile *.el files and org-mode:
-
-/usr/bin/emacs -batch -l ${FILE_WITH_EL_FILES} -kill && \
-cd contrib/org-mode/ && \
-make clean && make && make autoloads && make doc
+## 2025-03-13: disabled because I'm no longer using custom org-mode version:
+# /usr/bin/emacs -batch -l ${FILE_WITH_EL_FILES} -kill && \
+# cd contrib/org-mode/ && \
+# make clean && make && make autoloads && make doc
 
 #end
